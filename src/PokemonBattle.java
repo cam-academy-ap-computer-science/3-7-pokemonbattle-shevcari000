daa8325b2a4ec864028ee65434d31a14e80c6b2c
shevcari000 <shevcari000@CAM-LAB-29.bgsd.k12.wa.us>
2019-11-01 07:48:36 -0700

    Started.

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,9 +1,19 @@
-
+import java.util.*;
 public class PokemonBattle {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	
 	}
-
+	public static String battlestart() {
+		// TODO Auto-generated method stub
+		Scanner console = new Scanner(System.in);
+		
+		System.out.print("Another trainer is issuing a challenge!/Zebstrika appeared./Which pokemon do you choose?");
+		
+		String name = console.nextLine();
+		
+		System.out.println("You chose ")
+		return name();
+		
+	}
 }
