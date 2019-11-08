484e279416746cd1b76adf2b236d24697078b2c0
shevcari000 <shevcari000@CAM-LAB-29.bgsd.k12.wa.us>
2019-11-08 07:48:40 -0800

    ok

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,19 +1,24 @@
 import java.util.*;
+
 public class PokemonBattle {
-	public static void main(String[] args) {
-	
-	
+	private static void Scanner() {
 	}
+
+	public static void main(String[] args) {
+
+	}
+
 	public static String battlestart() {
 		// TODO Auto-generated method stub
 		Scanner console = new Scanner(System.in);
 		
 		System.out.print("Another trainer is issuing a challenge!/Zebstrika appeared./Which pokemon do you choose?");
 		
-		String name = console.nextLine();
+		System.out.println("You chose Alakazam!");
 		
-		System.out.println("You chose ")
-		return name();
+		System.out.println("It’s a Pokemon battle between Alakazam and Zebstrika!");
+		return null;
 		
 	}
 }
+
