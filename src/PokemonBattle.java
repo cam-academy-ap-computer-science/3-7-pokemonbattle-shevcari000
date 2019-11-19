7b5dccb94ecc6b44750a33f0422d1e04616388f0
shevcari000 <shevcari000@CAM-LAB-29.bgsd.k12.wa.us>
2019-11-19 07:46:31 -0800

    Done/

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,24 +1,56 @@
 import java.util.*;
 
 public class PokemonBattle {
-	private static void Scanner() {
+	static Scanner userInput = new Scanner (System.in);
+	public static void main(String [] args) {
+		String username = battleStart();
+		int hp = Damage(username);
+		StatsTable(username, hp);
 	}
-
-	public static void main(String[] args) {
-
+	public static String battleStart() {
+		System.out.println ("Another trainer is issuing a challenge!/Zebstrika appeared./Which Pokemon do you choose?");
+			String username = userInput.nextLine();
+			System.out.println("You choose " + username + "!");
+			System.out.println("It's a Pokemon battle betweem Alakazam and Zabstrika! Go!");
+			System.out.println();
+			return username;
 	}
-
-	public static String battlestart() {
-		// TODO Auto-generated method stub
-		Scanner console = new Scanner(System.in);
+	
+	public static int Damage(String username) {
+		System.out.println("Zebstrika used Thunderbolt!");
+		System.out.println("Trainer what are your " + username + "'s stats");
+		System.out.print("Level: ");
+		int Level = userInput.nextInt();
+		System.out.print("Attack: ");
+		int Attack = userInput.nextInt();
+		System.out.print("Defense: ");
+		int Defense = userInput.nextInt(); 
+		System.out.print("Base: ");
+		int Base = userInput.nextInt();
+		System.out.print("Stab: ");
+		int Stab = userInput.nextInt();
+		System.out.print("HP ");
+		int HP = userInput.nextInt();
+		System.out.println();
+		double Modifier = (0.85 + (Math.random() * 0.15)) * Stab;
+		int Damage = (int)((((2 * Level + 10) / (250)) + ((Attack/Defense) * Base + 2) * Modifier);
+		int hp = HP - 10;
+		System.out.println("HP, after damage, is now" + hp);
+		System.out.println();
+		return hp;
 		
-		System.out.print("Another trainer is issuing a challenge!/Zebstrika appeared./Which pokemon do you choose?");
-		
-		System.out.println("You chose Alakazam!");
-		
-		System.out.println("It’s a Pokemon battle between Alakazam and Zebstrika!");
-		return null;
+	}
+	public static void StatsTable(String username, int hp) {
+		System.out.println ("Name   " + username);
+		System.out.println ("Level   40");
+		System.out.println ("------------------------------");
+		System.out.println ("HP   " + hp);
+		System.out.println ("Attack   51");
+		System.out.println ("Defense  51");
+		System.out.println ("Sp. Atk 121");
+		System.out.println ("Sp. Def  81");
+		System.out.println ("Speed   107");
+		System.out.println ("------------------------------");
 		
 	}
 }
-
